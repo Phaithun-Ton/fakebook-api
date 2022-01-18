c74675a3cc29f978fd63bcef633a310fc96eefc3
Phaithun-Ton <phaithun.ap@gmail.com>
2022-01-18 09:27:30 +0700

    'fakebook-api'

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,9 @@ const { User } = require("../models");
 exports.updateProfileImg = async (req, res, next) => {
   try {
     console.log(req.file);
+
+    cloudinary.uploader.upload(req.file.path, (err, result) => {});
+
     await User.update(
       { profileImg: req.file.path },
       { where: { id: req.user.id } }
